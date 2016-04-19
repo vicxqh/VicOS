0d71fb207bf476396a13916c72bf2f1caabf90ab
Vic Xiao <xiaowei1235@gmail.com>
2016-04-19 17:15:55 +0800

    Change os name & Corrected cross-compiler

--- cross/i686-elf/lib/ldscripts/elf_i386.xsw
+++ cross/i686-elf/lib/ldscripts/elf_i386.xsw
@@ -7,7 +7,7 @@ OUTPUT_FORMAT("elf32-i386", "elf32-i386",
 	      "elf32-i386")
 OUTPUT_ARCH(i386)
 ENTRY(_start)
-SEARCH_DIR("/Users/Vic/opt/cross/i686-elf/lib");
+SEARCH_DIR("=/Users/Vic/opt/cross/i686-elf/lib"); SEARCH_DIR("=/usr/local/lib"); SEARCH_DIR("=/lib"); SEARCH_DIR("=/usr/lib");
 SECTIONS
 {
   /* Read-only sections, merged into text segment: */
--- src/VicOS/iso.sh
+++ src/VicOS/iso.sh
@@ -6,10 +6,10 @@ mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
-cp sysroot/boot/myos.kernel isodir/boot/myos.kernel
+cp sysroot/boot/osv.kernel isodir/boot/osv.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "myos" {
-	multiboot /boot/myos.kernel
+menuentry "osv" {
+	multiboot /boot/osv.kernel
 }
 EOF
-grub-mkrescue -o myos.iso isodir
+grub-mkrescue -o osv.iso isodir
